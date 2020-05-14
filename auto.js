8b13164c7ad4bacc413a1116706eef424bf5c49f
Elyes <elyes.benamor@etudiant-enit.utm.tn>
2020-05-14 03:51:02 +0200

    getting some funtion for testing the api's

---
+++ auto.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+//let list = [];
+const GitlabApi = require('./api');
+
+const gitlabApi = new GitlabApi(process.env.TOKEN);
+(async () => {
+	try {
+		//const results = await gitlabApi.getLabelsByProjectId(17939843)
+		const projects = await gitlabApi.getProjects();
+		
+
+    } catch (err) {
+			console.log('Error ', err)
+	}
+})()
